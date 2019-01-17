ff9552bf58a4875fe2be6261a70fa0dd1ed38041
Martin McKeaveney <martinmckeaveney@gmail.com>
2019-01-17 22:02:56 +0000

    removing stack ghci folders

--- chapter3/exercises.hs
+++ chapter3/exercises.hs
@@ -3,3 +3,10 @@ thirdLetter s = s !! 2
 
 reverse' :: String -> String
 reverse' s = drop 3 s ++ take 1 (drop 2 s) ++ take 1 (drop 1 s) ++ take 1 s
+
+fibonnacci :: Integer -> [Integer]
+fibonnacci 0 = [0]
+fibonnacci 1 = [1] 
+fibonnacci n = fibonnacci (n-1) : fibonnacci (n-2)
+
+
